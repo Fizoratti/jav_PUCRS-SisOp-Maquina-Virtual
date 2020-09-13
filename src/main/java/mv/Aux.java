7c2206eca68a284d3a7210055f6482f4190e48dd
Fizoratti <robson.felipe@acad.pucrs.br>
2020-09-13 06:04:39 -0300

    Add método dump(Word[]) na classe Aux

--- src/main/java/mv/Aux.java
+++ src/main/java/mv/Aux.java
@@ -1,12 +1,16 @@
 package mv;
 
 public class Aux {
-    public void dump(Word w) {
-        System.out.print("[ "); 
-        System.out.print(w.opc); System.out.print(", ");
-        System.out.print(w.r1);  System.out.print(", ");
-        System.out.print(w.r2);  System.out.print(", ");
-        System.out.print(w.p);   System.out.println("  ] ");
+    public void dump(Word _word) {
+        System.out.println(_word.toString());
+    }
+
+    public void dump(Word[] _memory) {
+        for (int i = 0; 0 < _memory.length; i++) {
+            if(!Word.isEmpty(_memory[i])) {                                           // caso a Word tenha conteúdo
+                System.out.print(i); System.out.print(":  ");  dump(_memory[i]);  // exibe o conteúdo da Word
+            }
+        }
     }
 
     public void dump(Word[] m, int ini, int fim) {
@@ -15,13 +19,20 @@ public class Aux {
         }
     }
 
-    public void carga(Programa _p, Word[] _m) {
-        Word[] p = _p.read();
-        for (int i = 0; i < p.length; i++) {
-            _m[i].opc = p[i].opc;     
-            _m[i].r1 = p[i].r1;     
-            _m[i].r2 = p[i].r2;     
-            _m[i].p = p[i].p;
+    public void carga(Programa _programa, Word[] _memory) {
+        Word[] programa = _programa.read();
+        System.out.println("Memory lenght: " + _memory.length);
+        System.out.println("Memory[0]: " + _memory[0]);
+        System.out.println("Memory[1]: " + _memory[1]);
+        System.out.println("Memory[2]: " + _memory[2]);
+        System.out.println("Memory[3]: " + _memory[3]);
+        System.out.println("Programa lenght: " + programa.length);
+        for (int i = 0; i < programa.length; i++) {
+            _memory[i].opc = programa[i].opc;     
+            _memory[i].r1 = programa[i].r1;     
+            _memory[i].r2 = programa[i].r2;     
+            _memory[i].p = programa[i].p;
         }
+
     }
 }
