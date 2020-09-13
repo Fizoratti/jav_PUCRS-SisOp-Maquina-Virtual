bc0035da1ff3e3d08820381c68214927a207fc46
Fizoratti <robson.felipe@acad.pucrs.br>
2020-09-13 15:52:36 -0300

    Remove alguns debug logs

--- src/main/java/mv/Aux.java
+++ src/main/java/mv/Aux.java
@@ -40,20 +40,9 @@ public class Aux {
     }
 
     public void carga(Programa _programa, Word[] _memory) {
-        Word[] programa = _programa.read();
-                                            log.debug("Memory lenght: {}", _memory.length);
-                                            log.debug("Memory value in memory[0]: {}", _memory[0]);
-                                            log.debug("Memory value in memory[1]: {}", _memory[1]);
-                                            log.debug("Memory value in memory[2]: {}", _memory[2]);
-                                            log.debug("Memory value in memory[3]: {}", _memory[3]);
-
-                                            log.debug("Programa lenght: {}", programa.length);
-                                            log.debug("Memory value in memory[0]: {}", programa[0]);
-                                            log.debug("Memory value in memory[1]: {}", programa[1]);
-                                            log.debug("Memory value in memory[2]: {}", programa[2]);
-                                            log.debug("Memory value in memory[3]: {}", programa[3]);
-
                                             log.info("{} Loading program . . .", Aux.mark);
+        Word[] programa = _programa.read();
+
         for (int i = 0; i < programa.length; i++) {
             _memory[i].opc = programa[i].opc;     
             _memory[i].r1 = programa[i].r1;     
