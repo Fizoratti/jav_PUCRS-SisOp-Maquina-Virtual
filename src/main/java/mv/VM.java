fcb821b926302bd0ccd875e1ed71d1ce13b6d2eb
Fizoratti <robson.felipe@acad.pucrs.br>
2020-09-13 06:11:50 -0300

    Refatora a classe VM
    Adiciona o método initMemory(Word[]) que inicia uma memória do zero
    preenchendo palavras nulas em cada endereço da memória.
    
    Adiciona o método init(Programa[]) que inicia a máquina virtual carregando
    na memória e executando cada um dos programas.
    
    Enxuga o método run() movendo a carga do programa na memória para o
    método init(Programa[]).

--- src/main/java/mv/VM.java
+++ src/main/java/mv/VM.java
@@ -19,33 +19,89 @@ public class VM {
 	public int memorySize;    
     public Word[] memory;     
     public CPU cpu;    
-    public Aux aux;
+	public Aux aux;
 
     public VM(){
 		memorySize = 1024;
 		memory = new Word[memorySize];
-		for (int i=0; i<memorySize; i++) { 
-			memory[i] = new Word(Opcode.___,-1,-1,-1); 
-		};
+		initMemory(memory);
+		System.out.println("Memory memory[0]: " + memory[0]);
+		System.out.println("Memory memory[1]: " + memory[1]);
+		System.out.println("Memory memory[2]: " + memory[2]);
+		System.out.println("Memory memory[3]: " + memory[3]);
 		cpu = new CPU(memory);
 		aux = new Aux();
+		aux.dump(memory);
 	}
 	
-	private void run(Programa _programa) {
-		Programa p = _programa;
-		aux.carga(p, memory);
-		cpu.setContext(0, memorySize, 0);
-		System.out.println("---------------------------------- programa carregado ");
-		aux.dump(memory, 0, 15);
-		System.out.println("---------------------------------- após execucao ");
+
+	/**
+	 * Executa o conteúdo presente na memória (instruções e dados de um programa).
+	 */
+	private void run() {
+		aux.dump(memory);
 		cpu.run();
-		aux.dump(memory, 0, 63);
+		aux.dump(memory);
 	}
 
+
+	/**
+	 * Inicia a máquina virtual carregando na memória e executando cada um dos programas.
+	 * @param _programas a lista de programas que a mv deve executar
+	 */
 	public void init(Programa[] _programas) {
-		for (Programa p : _programas) {
-            run(p);
+		cpu.setContext(0, memorySize, 0);
+		// Para cada programa da lista...
+		for (Programa programa : _programas) {
+			// Carrega na memoria...
+			aux.carga(programa, memory);
+			// Executa o programa
+            run();
         }
 	}
+	
+
+
+
+	/**
+	 * Inicia uma memória do zero preenchendo valores nulos em cada endereço.
+	 * @param _memory instância de memória que ainda não possui conteúdo
+	 */
+	private void initMemory(Word[] _memory) {
+		// Caso a memória não tenha conteúdo
+		if(isMemoryEmpty(_memory))
+			// Preenche a memória com conteúdo nulo
+			fillMemory(memory, new Word(Opcode.___,-1,-1,-1));
+	}
+
+
+	/**
+	 * Sobrescreve todos os endereços da memória com a palavra recebida.
+	 * @param _memory a memória da CPU
+	 * @param _word	a palavra que será escrita em todos os endereços da memória
+	 */
+	private void fillMemory(Word[] _memory, Word _word) {
+		// Para cada endereço na memória...
+		for(int i = 0; i < _memory.length; i++) {
+			// Escrever a palavra		
+			_memory[i] = _word; 
+		};
+	}
    
+
+
+	private boolean isMemoryEmpty(Word[] _memory) {
+		boolean isMemoryEmpty = true;
+
+		// for (Word word : _memory) {
+		// 	if(word != null) {
+		// 		isMemoryEmpty = false;
+		// 		break;
+		// 	}
+		// }
+		
+		return isMemoryEmpty;
+	}
+
+
 }
